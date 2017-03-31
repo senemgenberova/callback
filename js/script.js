6183446855869cb0f0a73ce420a03e59da9544da
senem qenberova <senemgenberova@gmail.com>
2017-03-31 21:34:47 +0400

    commit call

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@ var circle = document.getElementById("circle");
 var img = document.getElementById("img");
 var n = Number(prompt("Enter seconds"));
 
-circle.addEventListener("click",function(callback) {
-	circle.style.border = "5px solid #0078D7";
-	var set_time =setTimeout(boyut,n*1000);
+circle.addEventListener("click", function() {
+    border(boyut);
+})
 
-}) 
+function border(callback) {
+    circle.style.border = "5px solid #0078D7";
+    callback();
 
-function boyut(){
-	img.style.width = "235px";
-	img.style.transition = "all 1s ease-in-out";
-	circle.style.border = "none";
+}
+
+function boyut() {
+    setTimeout(function() {
+        img.style.width = "235px";
+        img.style.transition = "all 1s ease-in-out";
+        circle.style.border = "none";
+    }, n * 1000);
 }
